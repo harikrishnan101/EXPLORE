2021e28491f5c78404be7d01d78581c7db5df33f
HARIKRISHNAN R <harikrishnan183hari@gmail.com>
2023-10-19 12:02:59 +0530

    validate

--- routes/user.js
+++ routes/user.js
@@ -4,13 +4,13 @@ const router=express.Router()
 const {doSignup,loginPage,signup,doLogin,homePage,detailedview,logout,createBlog,addBlogData}=require('../controllers/userController')
 const userauth=require( '../middleware/userauth')
 
-router.get('/',loginPage);
-router.get('/signup',signup);
-router.post('/register',doSignup);
-router.post('/login',doLogin);
-router.get('/home',userauth,homePage);
-router.get('/detailedview',userauth,detailedview);
-router.get('/logout',logout);
-router.get('/createBlog',userauth,createBlog);
-router.post('/createBlog',userauth,addBlogData);
+router.get('/',loginPage)
+router.get('/signup',signup)
+router.post('/register',doSignup)
+router.post('/login',doLogin)
+router.get('/home',userauth,homePage)
+router.get('/detailedview',userauth,detailedview)
+router.get('/logout',logout)
+router.get('/createBlog',userauth,createBlog)
+router.post('/createBlog',userauth,addBlogData)
 module.exports=router
